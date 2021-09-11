ca91e82dd30709bf3e06285186474740f499f5e2
kokeDiaz <jorg.diazm@duocuc.cl>
2021-09-11 10:42:59 -0300

    Confirmar3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./Pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'pagina1',
+    loadChildren: () => import('./Pages/pagina1/pagina1.module').then( m => m.Pagina1PageModule)
   },
 ];
 
